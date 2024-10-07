dbc4315e09542f4529209580eabefdb08179ec56
Radim Janalík <radim.janalik@gmail.com>
2024-10-07 18:24:26 +0300

    Fix module

--- modules/spack/24.03.lua
+++ modules/spack/24.03.lua
@@ -1,14 +1,17 @@
+local spackver = "0.22.0"
+local cpever = "24.03"
+
 help( [[
 A module to activate Spack and configure it to install packages in a directory specified by the user in the environment variable $SPACK_USER_PREFIX. The set-up is such that you use a common central Spack instance in /appl/lumi/spack, but you install packages in your own directory. Thus, you cannot change the configurations and update the Spack package repository. This has to be done by LUMI User Support Team . But you save the hassle of having to clone your own spack directory and write the configuration files.
 ]]
 )
-whatis("Version: 23.09")
+whatis("Version: " .. spackver)
 whatis("Keywords: Spack")
-whatis("Description: the Spack package manager configured to install in a user specified directory. Spack release version 0.21.0 with minor patches and backported bugfixes. CPE 23.09 compilers and libraries.")
+whatis("Description: the Spack package manager configured to install in a user specified directory. Spack release version " .. spackver .. " with minor patches and backported bugfixes. CPE " .. cpever .. " compilers and libraries.")
 
 family( 'LUMI_SoftwareStack' )
 
-local spackroot = "/appl/lumi/spack/23.09/0.21.0-user"
+local spackroot = "/appl/lumi/spack/" .. cpever .. "/" .. spackver
 local userdir = os.getenv("SPACK_USER_PREFIX")
 
 require("lfs")
@@ -49,20 +52,20 @@ if mode() == "load" then
   end
 
   -- Check for the modules directory and try to create it
-  local moduledir = userdir .. "/23.09/0.21.0/modules/tcl"
+  local moduledir = userdir .. "/" .. cpever .. "/" .. spackver .. "/modules/tcl"
   if not isDir(moduledir) then
-    LmodMessage("Creating the Spack modules directory " .. (userdir .. "/23.09/0.21.0/modules/tcl"))
-    ok,_,_ = os.execute("mkdir -p " .. userdir .. "/23.09/0.21.0/modules/tcl")
+    LmodMessage("Creating the Spack modules directory " .. (userdir .. "/" .. cpever .. "/" .. spackver .. "/modules/tcl"))
+    ok,_,_ = os.execute("mkdir -p " .. userdir .. "/" .. cpever .. "/" .. spackver .. "/modules/tcl")
     if not ok then
       LmodError("The modules directory (" .. moduledir .. ") specified in $SPACK_USER_PREFIX does not exist and the Spack module tried to create it, but it did not work.")
     end
   end
 
   -- Check for the cache directory and try to create it
-  local cachedir = userdir .. "/23.09/0.21.0/cache"
+  local cachedir = userdir .. "/" .. cpever .. "/" .. spackver .. "/cache"
   if not isDir(cachedir) then
-    LmodMessage("Creating the Spack cache directory " .. (userdir .. "/23.09/0.21.0/cache"))
-    ok,_,_ = os.execute("mkdir -p " .. userdir .. "/23.09/0.21.0/cache")
+    LmodMessage("Creating the Spack cache directory " .. (userdir .. "/" .. cpever .. "/" .. spackver .. "/cache"))
+    ok,_,_ = os.execute("mkdir -p " .. userdir .. "/" .. cpever .. "/" .. spackver .. "/cache")
     if not ok then
       LmodError("The cache directory (" .. cachedir .. ") specified in $SPACK_USER_PREFIX does not exist and the Spack module tried to create it, but it did not work.")
     end
@@ -82,7 +85,7 @@ setenv("SPACK_ROOT",spackroot)
 setenv("SPACK_DISABLE_LOCAL_CONFIG","true")
 
 -- Add Spack's modules
-prepend_path("MODULEPATH",userdir .. "/23.09/0.21.0/modules/tcl/linux-sles15-zen")
-prepend_path("MODULEPATH",userdir .. "/23.09/0.21.0/modules/tcl/linux-sles15-zen2")
-prepend_path("MODULEPATH","/appl/lumi/spack/23.09/0.21.0/share/spack/modules/linux-sles15-zen")
-prepend_path("MODULEPATH","/appl/lumi/spack/23.09/0.21.0/share/spack/modules/linux-sles15-zen2")
+prepend_path("MODULEPATH",userdir .. "/" .. cpever .. "/" .. spackver .. "/modules/tcl/linux-sles15-zen")
+prepend_path("MODULEPATH",userdir .. "/" .. cpever .. "/" .. spackver .. "/modules/tcl/linux-sles15-zen2")
+prepend_path("MODULEPATH","/appl/lumi/spack/" .. cpever .. "/" .. spackver .. "/share/spack/modules/linux-sles15-zen")
+prepend_path("MODULEPATH","/appl/lumi/spack/" .. cpever .. "/" .. spackver .. "/share/spack/modules/linux-sles15-zen2")
